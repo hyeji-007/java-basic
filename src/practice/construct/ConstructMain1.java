43dad7d0dd688f8748e3af0906da883a9fb1953f
Hyeji Kwon <kwonhyeji007@gmail.com>
2024-10-19 18:03:03 +0900

    study at home

--- src/practice/construct/ConstructMain1.java
+++ src/practice/construct/ConstructMain1.java
@@ -3,7 +3,7 @@ package practice.construct;
 public class ConstructMain1 {
     public static void main(String[] args) {
         MemberConstruct member1 = new MemberConstruct("user1", 15, 90);
-        MemberConstruct member2 = new MemberConstruct("user2", 16, 80);
+        MemberConstruct member2 = new MemberConstruct("user2", 16);
 
         MemberConstruct[] members = {member1, member2};
 
--- src/practice/construct/MemberConstruct.java
+++ src/practice/construct/MemberConstruct.java
@@ -5,6 +5,11 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    //추가
+    MemberConstruct(String name, int age) {
+        this(name, age, 50); //변경
+    }
+
     MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name=" + name + ", age=" + age + ", grade" + grade);
 
