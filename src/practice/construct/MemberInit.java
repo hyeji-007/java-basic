5d86fb363c7cf42d5faf23f3bceff881d86823c8
Hyeji Kwon <kwonhyeji007@gmail.com>
2024-10-19 16:07:22 +0900

    study at home

--- src/practice/construct/MemberInit.java
+++ src/practice/construct/MemberInit.java
@@ -4,4 +4,12 @@ public class MemberInit {
     String name;
     int age;
     int grade;
+
+    //추가
+    void initMember(String name, int age, int grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
 }
