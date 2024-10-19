483dc057bcb5699a6bd2f2de104b9cad357571b2
Hyeji Kwon <kwonhyeji007@gmail.com>
2024-10-19 22:30:58 +0900

    study at home

---
+++ src/practice/access/SpeakerMain.java
@@ -0,0 +1,21 @@
+package practice.access;
+
+public class SpeakerMain {
+
+    public static void main(String[] args) {
+        Speaker speaker = new Speaker(90);
+        speaker.showVolume();
+
+        speaker.volumeUp();
+        speaker.showVolume();
+
+        speaker.volumeUp();
+        speaker.showVolume();
+
+        //필드(변수)에 직접 접근
+        System.out.println("volume 필드 직접 접근 수정");
+        //speaker.volume = 200; //private 접근 오류
+        speaker.showVolume();
+
+    }
+}
