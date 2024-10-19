aa06d78a9d4d3bfcd6330895dcc232f3192eb6ad
Hyeji Kwon <kwonhyeji007@gmail.com>
2024-10-19 12:27:32 +0900

    study at home

---
+++ src/practice/oop1/Rectangle.java
@@ -0,0 +1,19 @@
+package practice.oop1;
+
+public class Rectangle {
+    int width;
+    int height;
+
+    int calculateArea() {
+        return width * height;
+    }
+
+    int calculatePerimeter() {
+        return 2 * (width + height);
+    }
+
+    boolean isSquare() {
+        return this.width == this.height;
+    }
+
+}
