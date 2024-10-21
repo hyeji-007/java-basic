264d5100e6a81800ce09e2caa97b24028bfcc98c
Hyeji Kwon <kwonhyeji007@gmail.com>
2024-10-22 08:41:56 +0900

    study at home

---
+++ src/practice/extends1/overriding/Car.java
@@ -0,0 +1,12 @@
+package practice.extends1.overriding;
+
+public class Car {
+
+    public void move() {
+        System.out.println("차를 이동합니다.");
+    }
+
+    public void openDoor() {
+        System.out.println("문을 엽니다.");
+    }
+}
