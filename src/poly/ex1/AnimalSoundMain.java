d186d46077cac13ee9de3edeed2944a129969445
Hyeji Kwon <kwonhyeji007@gmail.com>
2024-10-26 23:38:33 +0900

    study at home

--- src/poly/ex1/AnimalSoundMain.java
+++ src/poly/ex1/AnimalSoundMain.java
@@ -11,13 +11,19 @@ public class AnimalSoundMain {
         dog.sound();
         System.out.println("동물 소리 테스트 종료");
 
+        soundCat(cat);
+        soundCaw(caw);
+    }
+
+    private static void soundCat(Cat cat) {
         System.out.println("동물 소리 테스트 시작");
         cat.sound();
         System.out.println("동물 소리 테스트 종료");
+    }
 
+    private static void soundCaw(Caw caw) {
         System.out.println("동물 소리 테스트 시작");
         caw.sound();
         System.out.println("동물 소리 테스트 종료");
-
     }
 }
