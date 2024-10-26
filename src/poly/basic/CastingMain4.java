7d7c070bc448127658774261063e497a2a1bd322
Hyeji Kwon <kwonhyeji007@gmail.com>
2024-10-26 15:11:47 +0900

    study at home

--- src/poly/basic/CastingMain4.java
+++ src/poly/basic/CastingMain4.java
@@ -9,9 +9,7 @@ public class CastingMain4 {
         child1.childMethod(); //문제 없음
 
         Parent parent2 = new Parent();
-        Child child2 = (Child) parent2; //런타임 오류 - ClassCastException
+        Child child2 = (Child) parent2; //런타입 오류 - ClassCastException
         child2.childMethod(); //실행 불가
-
-
     }
 }
