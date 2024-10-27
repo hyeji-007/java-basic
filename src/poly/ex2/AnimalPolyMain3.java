50e30c6d4b25ae2a2948e242c1d41de2444e5f01
Hyeji Kwon <kwonhyeji007@gmail.com>
2024-10-27 12:06:13 +0900

    study at home

---
+++ src/poly/ex2/AnimalPolyMain3.java
@@ -0,0 +1,20 @@
+package poly.ex2;
+
+public class AnimalPolyMain3 {
+
+    public static void main(String[] args) {
+        Animal[] animalArr = {new Dog(), new Cat(), new Caw(), new Duck()}; //Cntl+Alt+N
+
+        for (Animal animal : animalArr) {
+            soundAnimal(animal); //Ctrl+Alt+M
+        }
+    }
+
+    //변하지 않는 부분
+    private static void soundAnimal(Animal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+
+}
